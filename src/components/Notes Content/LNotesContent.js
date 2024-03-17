d8be28d5969ed4f7c46191a27b829bc6f6ce69d8
Mohd Shoaib <145234853+Mohd-Sh0aib@users.noreply.github.com>
2024-03-17 17:05:41 +0530

    Add files via upload
    first commit

---
+++ src/components/Notes Content/LNotesContent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./LNotesContent.css";
+
+function LNotesContent({ note }) {
+  
+  return (
+   <div>
+     <div className="notes_content_note">
+       <div className="notes_content_details">
+         {note.content}
+       </div>
+      <div className="contentbox">
+      <div className="notes_content_date_time_details">
+          <div className="notes_content_time">{note.time}</div>
+          <div className="notes_content_date">{note.date}</div>
+       </div>
+      </div>
+    </div>
+   </div>
+  );
+}
+ 
+export default LNotesContent;
